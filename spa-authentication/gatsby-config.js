9d5d8ab863f0b2ad5cd5f09ac43818adfb1e4668
Roman Kosov <samerset14@gmail.com>
2021-12-13 22:35:29 +1000

    Delete `spa-authentication` folder

--- spa-authentication/gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    siteUrl: "https://samerset-rmn.github.io/spa-authentication/",
-    title: "spa-authentication",
-  },
-  plugins: ["gatsby-plugin-emotion"],
-  pathPrefix: "/spa-authentication",
-};
--- spa-authentication/public/component---src-pages-index-js-e9496675ce5e017bf030.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkspa_authentication=self.webpackChunkspa_authentication||[]).push([[678],{7704:function(n,t,u){u.r(t);u(7294);var a=u(9021);t.default=function(){return(0,a.tZ)("main",null,(0,a.tZ)("h1",null,"Hello, World"))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-e9496675ce5e017bf030.js.map
